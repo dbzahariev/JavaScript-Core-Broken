6c6eb3739698c75065e024c02ee44201e5220961
dbzahariev <dbzahariev@gmail.com>
2018-02-27 10:59:08 +0200

    Add new files

---
+++ JavaScript-Advanced/L3: Advanced Functions/P02. Currency Format.js
@@ -0,0 +1,15 @@
+function currencyFormatter(separator, symbol, symbolFirst, value) {
+    let result = Math.trunc(value) + separator;
+    result += value.toFixed(2).substr(-2, 2);
+    if (symbolFirst) return symbol + ' ' + result;
+    else return result + ' ' + symbol;
+}
+
+let dollarFormatter = result(currencyFormatter)
+console.log(dollarFormatter(123.54))
+
+function result(func) {
+    return function (value) {
+        return func(',', '$', true, value)
+    }
+}
