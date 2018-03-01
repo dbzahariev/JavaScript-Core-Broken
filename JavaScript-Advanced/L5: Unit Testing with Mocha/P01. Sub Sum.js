6d3df41d34b3a8977e65e49eab3c25917bd7919d
dbzahariev <dbzahariev@gmail.com>
2018-03-01 18:36:12 +0200

    Add new files

---
+++ JavaScript-Advanced/L5: Unit Testing with Mocha/P01. Sub Sum.js
@@ -0,0 +1,11 @@
+function subSum(arr, start, end) {
+    if (!Array.isArray(arr)) {
+        return
+    }
+}
+
+subSum([10, 20, 30, 40, 50, 60], 3, 300)
+subSum([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1)
+subSum([10, 'twenty', 30, 40], 0, 2)
+subSum([], 1, 2)
+subSum('text', 0, 2)
---
+++ JavaScript-Advanced/L5: Unit Testing with Mocha/tests.js
@@ -0,0 +1,12 @@
+function test() {
+    // let str = 'Hello world'
+    // console.log(str[str.indexOf('')])
+
+    // let arr = new Array(1)
+    // arr.push('hi')
+    // arr.push('hi')
+    // arr.push('hi')
+    // console.log(arr);
+}
+
+test()
