a8bf36d2974fae6260ca6fed4b70b67555e7b379
dbzahariev <dbzahariev@gmail.com>
2018-03-01 18:53:58 +0200

    Add new files

--- JavaScript-Advanced/L5: Unit Testing with Mocha/P01. Sub Sum.js
+++ JavaScript-Advanced/L5: Unit Testing with Mocha/P01. Sub Sum.js
@@ -1,27 +1,19 @@
 function subSum(arr, start, end) {
     if (!Array.isArray(arr)) {
-        return
+        return NaN
     }
+    start = start < 0 ? 0 : start
+    end = end >= arr.length ? arr.length - 1 : end
 
-    let isError = false
-    arr.forEach(a => {
-        if (typeof a == 'string')
-            isError = true
-        }
-    )
-
-    if (isError == true){
-        return
+    let sum = 0
+    for (let i = start; i < end+1; i++) {
+        sum += Number(arr[i])
     }
-    console.log('hi')
-    if (start < 0) {
-        start = 0
-    }
-
+    return sum
 }
 
-// subSum([10, 20, 30, 40, 50, 60], 3, 300)
+console.log(subSum([10, 20, 30, 40, 50, 60], 3, 300));
 // subSum([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1)
-subSum([10, 'twenty', 30, 40], 0, 2)
+// subSum([10, 'twenty', 30, 40], 0, 2)
 // subSum([], 1, 2)
 // subSum('text', 0, 2)
