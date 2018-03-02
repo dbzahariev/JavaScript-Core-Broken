4cb7313aac7552cc9b1401f60c88b21b1826aa4d
dbzahariev <dbzahariev@gmail.com>
2018-03-02 13:50:22 +0200

    Add new files

---
+++ JavaScript-Advanced/L5: Unit Testing with Mocha/JJDD.js
@@ -0,0 +1,5 @@
+function kdsadas() {
+    console.log(`test`);
+}
+
+kdsadas();
---
+++ JavaScript-Advanced/L5: Unit Testing with Mocha/test/exampleTest.js
@@ -0,0 +1,26 @@
+let expect = require("chai").expect;
+
+function sum(arr) {
+    let sum = 0;
+    for (num of arr)
+        sum += Number(num);
+    return sum;
+}
+
+describe("sum(arr)", function () {
+    it("should return 3 for [1, 2]", function () {
+        expect(sum([1, 2])).to.be.equal(3);
+    });
+    it("should return 3 for [1, 2]", function () {
+        expect(sum([1, 2])).to.be.equal(3);
+    });
+});
+
+describe("hi", function () {
+    it("should return 3 for [1, 2]", function () {
+        expect(sum([1, 2])).to.be.equal(3);
+    });
+    it("should return 3 for [1, 2]", function () {
+        expect(sum([1, 2])).to.be.equal(3);
+    });
+})
