14957ef1285d94d32cd95041e67823e60805824f
Unknown <dbzahariev@gmail.com>
2018-03-30 19:58:32 +0300

    Add new files

---
+++ JavaScript-Apps/L04 - SPA/Book Library/scripts/crudOperations.js
@@ -0,0 +1,98 @@
+const BASE_URL = 'https://baas.kinvey.com/'
+const APP_KEY = 'kid_r1z4BCl9f'
+const APP_SECRET = '379bf4520d7643afbdf3c3de1373ac3c'
+const AUTH_HEADERS = {'Authorization': "Basic " + btoa(APP_KEY + ":" + APP_SECRET)}
+const BOOKS_PER_PAGE = 10
+
+function loginUser() {
+    // TODO
+    // POST -> BASE_URL + 'user/' + APP_KEY + '/login'
+    // signInUser(res, 'Login successful.')
+}
+
+function registerUser() {
+    let username = $("#formRegister > div:nth-child(2) > input:nth-child(1)").val()
+    let password = $("#formRegister > div:nth-child(4) > input:nth-child(1)").val()
+
+    $.ajax({
+        type: "POST",
+        url: bassURL + 'user/' + appKey + '/',
+        data: {username, password},
+        success: signInUser(data, 'Registration successful.'),
+        error: handleAjaxError
+        // dataType: dataType
+    });
+}
+
+function listBooks() {
+    // TODO
+    // GET -> BASE_URL + 'appdata/' + APP_KEY + '/books'
+    // displayPaginationAndBooks(res.reverse())
+}
+
+
+function createBook() {
+    // TODO
+    // POST -> BASE_URL + 'appdata/' + APP_KEY + '/books'
+    // showInfo('Book created.')
+}
+
+function deleteBook(book) {
+    // TODO
+    // DELETE -> BASE_URL + 'appdata/' + APP_KEY + '/books/' + book._id
+    // showInfo('Book deleted.')
+}
+
+function loadBookForEdit(book) {
+    // TODO
+}
+
+function editBook() {
+    // TODO
+    // PUT -> BASE_URL + 'appdata/' + APP_KEY + '/books/' + book._id
+    // showInfo('Book edited.')
+}
+
+function saveAuthInSession(userInfo) {
+    // TODO
+}
+
+function logoutUser() {
+    // TODO
+    // showInfo('Logout successful.')
+}
+
+function signInUser(res, message) {
+    // TODO
+}
+
+function displayPaginationAndBooks(books) {
+    let pagination = $('#pagination-demo')
+    if (pagination.data("twbs-pagination")) {
+        pagination.twbsPagination('destroy')
+    }
+    pagination.twbsPagination({
+        totalPages: Math.ceil(books.length / BOOKS_PER_PAGE),
+        visiblePages: 5,
+        next: 'Next',
+        prev: 'Prev',
+        onPageClick: function (event, page) {
+            // TODO remove old page books
+            let startBook = (page - 1) * BOOKS_PER_PAGE
+            let endBook = Math.min(startBook + BOOKS_PER_PAGE, books.length)
+            $(`a:contains(${page})`).addClass('active')
+            for (let i = startBook; i < endBook; i++) {
+                // TODO add new page books
+            }
+        }
+    })
+}
+
+function handleAjaxError(response) {
+    let errorMsg = JSON.stringify(response)
+    if (response.readyState === 0)
+        errorMsg = "Cannot connect due to network error."
+    if (response.responseJSON && response.responseJSON.description)
+        errorMsg = response.responseJSON.description
+    showError(errorMsg)
+}
